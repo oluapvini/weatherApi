48f7166a42e21bb57c419853b9c877007bbfe527
Paulo Vinícius do Nascimento <vinioluap02@gmail.com>
2026-07-15 11:14:02 -0300

    feat: implement weather fetching and response formatting functions

--- src/server.js
+++ src/server.js
@@ -1,18 +1,37 @@
 import express from "express";
 import "dotenv/config";
 import { env } from "./config/env.js";
+import { fetchWeatherByCity, formatWeatherResponse } from "./clients/visual-crossing.client.js";
 
 const app = express();
-const PORT = env.port;
 
-app.get("/weather/:city", (req, res) => {
-    const city = req.params.city;
-    
-    res.json({
-        city,
-        temperature: 25, 
-        condition: "Sunny"
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Weather API is running",
     });
 });
 
-app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`));
+app.get("/weather/:city", async (req, res) => {
+try {
+        const { city } = req.params;
+
+        if (!city) {
+            return res.status(400).json({
+                message: "City is required.",
+            });
+        }
+
+        const weatherData = await fetchWeatherByCity(city);
+        const formattedWeather = await formatWeatherResponse(weatherData);
+
+        return res.json(formattedWeather);
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Failed to fetch weather data.",
+        });
+    }
+});
+
+app.listen(env.port, () => console.log(`O servidor está rodando na porta ${env.port}`));
